10cf56e86dd9f4b57bb54e2c5802b09fb313d970
William Babincsak <58189391+TheWillBa@users.noreply.github.com>
2023-04-25 17:11:37 -0400

    make baseline_eff and add safe_reset for running baselines

--- src/baseline_eff.py
+++ src/baseline_eff.py
@@ -7,10 +7,11 @@ import minerl
 import pickle
 import pandas as pd
 import time
-from util import safe_reset
+from efficient_vpt import EfficientVPT
+import torch
 
 import numpy as np
-
+from util import safe_reset
 
 import matplotlib.pyplot as plt
 
@@ -21,7 +22,8 @@ import sys
 from tqdm import tqdm
 
 sys.path.insert(0, "vpt")  # nopep8
-from agent import MineRLAgent  # nopep8
+
+from lib.tree_util import tree_map  # nopep8
 
 
 def main(
@@ -32,9 +34,9 @@ def main(
 ):
     env_name = env
     model_path = f"models/{model}.model"
-    weights_path = f"weights/{weights}.weights"
+    weights_path = f"noted_weights/{weights}.weights"
 
-    baseline_name = f"{env_name}&{model}&{weights}"
+    baseline_name = f"{env_name}&{model}&{weights}-2"
 
     print(f"Beginning baseline (n={n})")
     print("============================")
@@ -49,10 +51,10 @@ def main(
     pi_head_kwargs = agent_parameters["model"]["args"]["pi_head_opts"]
     pi_head_kwargs["temperature"] = float(pi_head_kwargs["temperature"])
 
-    agent = MineRLAgent(env, policy_kwargs=policy_kwargs,
-                        pi_head_kwargs=pi_head_kwargs)
+    vpt = EfficientVPT(env, policy_kwargs=policy_kwargs,
+                        pi_head_kwargs=pi_head_kwargs, use_skip=True)
 
-    agent.load_weights(weights_path)
+    vpt.load_weights(weights_path)
 
     plt.ion()
     fig = plt.figure()
@@ -64,19 +66,33 @@ def main(
     killed = []
     damage = []
 
+        # This is a dummy tensor of shape (batchsize, 1) which was used as a mask internally
+    dummy_first = torch.from_numpy(np.array((False,)))
+    dummy_first = dummy_first.unsqueeze(1)
+
     for eps in tqdm(range(n)):
         # # Hard reset every 10 episodes so we don't crash
         # if eps % 10 == 0 and eps > 0:
         #     env.close()
         #     env = gym.make(env_name)
-
-        agent.reset()
         obs, env = safe_reset(env)
+        hidden_state = vpt.initial_state(1)
 
         done = False
         total_reward = 0
         while not done:
-            action = agent.get_action(obs)
+            agent_obs = vpt._env_obs_to_agent(obs)
+            # Basically just adds a dimension to both camera and button tensors
+            agent_obs = tree_map(lambda x: x.unsqueeze(1), agent_obs)
+
+            latent, hidden_state = vpt.run_vpt_base(agent_obs, hidden_state, dummy_first)
+            pi_distribution = vpt.get_policy(latent)
+
+            action = vpt.policy.pi_head.sample(
+                pi_distribution, deterministic=False)
+            
+            action = vpt._agent_action_to_env(action)
+            
             obs, reward, done, info = env.step(action)
             total_reward += reward
             # env.render()
